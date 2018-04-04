80b6ce7eba468b4e2e734f7a0ee29367c45a25ff
Martim Marques <martimasterdeveloper@gmail.com>
2018-04-04 16:50:24 +0100

    Make explorer resizable

--- src/main/java/org/neon/api/explorer/NeonExplorer.kt
+++ src/main/java/org/neon/api/explorer/NeonExplorer.kt
@@ -2,6 +2,7 @@ package org.neon.api.explorer
 
 import javafx.scene.control.TreeView
 import org.neon.Main
+import org.neon.util.controls.Resizable
 import org.neon.util.files.NeonFile
 
 object NeonExplorer : TreeView<NeonFile>() {
@@ -9,6 +10,7 @@ object NeonExplorer : TreeView<NeonFile>() {
     init {
         this.prefHeight = Main.screen.height - 23.5
         this.layoutY += 23.5
+        Resizable(this).makeRegionResizable()
     }
 
 }
---
+++ src/main/java/org/neon/util/controls/Resizable.java
@@ -0,0 +1,62 @@
+package org.neon.util.controls;
+
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+
+public class Resizable {
+
+    private final Region region;
+    private int resizeMargin = 5;
+    private double x;
+    private boolean dragging;
+
+    public Resizable(Region region) {
+        this.region = region;
+    }
+
+    public void makeRegionResizable() {
+
+        region.setOnMousePressed(event -> {
+            if (!isInDraggableZone(event)) {
+                return;
+            }
+            dragging = true;
+            region.setMinWidth(region.getWidth());
+            x = event.getX();
+        });
+
+        region.setOnMouseDragged(event -> {
+            if (dragging) {
+                if (region.getWidth() < 5) {
+                    resizeMargin = 1;
+                } else {
+                    resizeMargin = 5;
+                }
+                double mouseX = event.getX();
+                double newHeight = region.getMinWidth() + (mouseX - x);
+                region.setMinWidth(newHeight);
+                x = mouseX;
+            }
+        });
+
+        region.setOnMouseMoved(event -> {
+            if (isInDraggableZone(event) || dragging) {
+                region.setCursor(Cursor.E_RESIZE);
+            } else {
+                region.setCursor(Cursor.DEFAULT);
+            }
+        });
+
+        region.setOnMouseReleased(event -> {
+            dragging = false;
+            region.setCursor(Cursor.DEFAULT);
+        });
+
+    }
+
+    private boolean isInDraggableZone(MouseEvent event) {
+        return event.getX() > (region.getWidth() - resizeMargin);
+    }
+
+}
